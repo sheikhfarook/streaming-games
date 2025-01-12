bc0887bf7387651928d5a68570d047b481ccc6b2
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2025-01-12 11:20:05 +0530

    change the components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import { Button } from "@/components/ui/button";
+import { Button } from "@/app/component/ui/button";
 
 export default function Home() {
   return (
-    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Button>Hello Button</Button>
     </div>
   );
